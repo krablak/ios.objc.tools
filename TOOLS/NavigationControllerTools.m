16eddfcd3b1694c6971359ff48111d4e0ef3d82f
krablak <krablak@gmail.com>
2012-12-11 20:14:09 +0100

    Added new utility classes.

---
+++ TOOLS/NavigationControllerTools.m
@@ -0,0 +1,13 @@
+//
+//  NavigationControllerTools.m
+//  TOOLS
+//
+//  Created by Michal Racek on 12/4/12.
+//  Copyright (c) 2012 Michal Racek. All rights reserved.
+//
+
+#import "NavigationControllerTools.h"
+
+@implementation NavigationControllerTools
+
+@end
